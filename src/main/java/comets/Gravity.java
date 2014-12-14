f6cfbd0bc3d61fdc0396732e984401e2be9b4da4
Paweł Koniarski <me@pkoniarski.com>
2014-12-14 18:39:07 +0100

    Changed variable to constant (capitalised name and added final).

--- src/main/java/comets/Gravity.java
+++ src/main/java/comets/Gravity.java
@@ -2,12 +2,11 @@ package comets;
 
 public class Gravity {
 
-    public static double gravitational_constant = 6.67384e-11;
+    public static final double GRAVITATIONAL_CONSTANT = 6.67384e-11;
 
     public static double calculateForce(SpaceObject a, SpaceObject b) {
 
         double distance = a.getPosition().distance(b.getPosition());
-        return gravitational_constant * a.getMass() * b.getMass() / (distance*distance);
-
+        return GRAVITATIONAL_CONSTANT * a.getMass() * b.getMass() / (distance*distance);
     }
 }
