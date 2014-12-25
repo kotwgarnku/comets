4a8d3f48a8fe9285c194a86a95beeac6826f294f
Filip <filkonieczny@gmail.com>
2014-12-26 00:11:20 +0100

    Changed calculating Force so it is a vector.

--- src/main/java/comets/Gravity.java
+++ src/main/java/comets/Gravity.java
@@ -1,12 +1,15 @@
 package comets;
 
+import javafx.geometry.Point3D;
+
 public class Gravity {
 
     public static final double GRAVITATIONAL_CONSTANT = 6.67384e-11;
 
-    public static double calculateForce(SpaceObject a, SpaceObject b) {
+    public static Point3D determineForceVector(SpaceObject a, SpaceObject b) {
 
         double distance = a.getPosition().distance(b.getPosition());
-        return GRAVITATIONAL_CONSTANT * a.getMass() * b.getMass() / (distance*distance);
+        Point3D ForceVector = b.getPosition().subtract(a.getPosition());
+        return ForceVector.multiply(b.getMass()/Math.pow(distance, 3));
     }
 }
