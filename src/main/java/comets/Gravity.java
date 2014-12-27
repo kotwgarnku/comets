ab33e899a0200141685b930d2ae67f77f6e66929
Paweł Koniarski <me@pkoniarski.com>
2014-12-27 04:03:32 +0100

    Fixed the possible division by zero in Gravity class.

--- src/main/java/comets/Gravity.java
+++ src/main/java/comets/Gravity.java
@@ -9,10 +9,13 @@ public class Gravity implements Force {
     @Override
     public Point3D calculateAcceleration(SpaceObject movingObject, SpaceObject standingObject) {
         Point3D differenceVector = standingObject.getPosition().subtract(movingObject.getPosition());
+        // TODO: change the code to be more 'vector friendly' (only if it won't make the code less clear)
         double x = differenceVector.getX();
         double y = differenceVector.getY();
         double z = differenceVector.getZ();
-        Point3D acceleration = new Point3D(Math.signum(x) / (x*x), Math.signum(y) / (y*y), Math.signum(z) / (z*z));
-        return acceleration.multiply(GRAVITATIONAL_CONSTANT * standingObject.getMass());
+        x = x != 0 ? Math.signum(x) / (x*x) : 0;
+        y = y != 0 ? Math.signum(y) / (y*y) : 0;
+        z = z != 0 ? Math.signum(z) / (z*z) : 0;
+        return (new Point3D(x, y, z)).multiply(GRAVITATIONAL_CONSTANT * standingObject.getMass());
     }
 }
