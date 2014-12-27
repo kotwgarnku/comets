63a60588542f20df114e78219151ed4021c40258
Paweł Koniarski <me@pkoniarski.com>
2014-12-27 01:53:52 +0100

    Moved the test for RungeKutta4 from RosettaCode to the class where all tests for RungeKutta4 should be.

--- src/test/java/comets/RungeKutta4Test.java
+++ src/test/java/comets/RungeKutta4Test.java
@@ -3,6 +3,7 @@ package comets;
 import org.junit.Test;
 
 import static java.lang.Math.exp;
+import static java.lang.Math.sqrt;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 
@@ -24,5 +25,25 @@ public class RungeKutta4Test {
             y = RungeKutta4.evaluate(x, y, dx, expFunction);
 
         assertThat(y).isCloseTo(exp(1), within(1E-10));
+    }
+
+    // http://rosettacode.org/wiki/Runge-Kutta_method
+    private class RosettaFunction implements Equation {
+
+        @Override
+        public double f(double x, double y) {
+            return x * sqrt(y);
+        }
+    }
+
+    @Test
+    public void calculatesCorrectResultForRosettaFunction() throws Exception {
+        double y = 1;
+        double dx = 0.005;
+        RosettaFunction rosFunction = new RosettaFunction();
+        for (double x = 0; x < 1; x += dx)
+            y = RungeKutta4.evaluate(x, y, dx, rosFunction);
+
+        assertThat(y).isCloseTo((25/16f), within(1E-10));
     }
 }
