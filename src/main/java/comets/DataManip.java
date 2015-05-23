851964f15569aeddc0f2666ede0f8bbeeeb7bbba
Paweł Koniarski <me@pkoniarski.com>
2015-05-24 01:12:46 +0200

    Refactored DataManip class, added radius to planets.txt and possibility to comment in planets.txt.

--- src/main/java/comets/DataManip.java
+++ src/main/java/comets/DataManip.java
@@ -2,43 +2,49 @@ package comets;
 
 import javafx.geometry.Point3D;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class DataManip {
     public static ArrayList<SpaceObject> parse(String fileName) {
         ArrayList<SpaceObject> spaceObjects = new ArrayList<>();
-        String name = "defaultName";
-        double posX = 0;
-        double posY = 0;
-        double posZ = 0;
-        double velX = 0;
-        double velY = 0;
-        double velZ = 0;
-        double mass = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] args = line.split("\\s+");
-                name = args[0];
-                posX = Double.parseDouble(args[1]);
-                posY = Double.parseDouble(args[2]);
-                posZ = Double.parseDouble(args[3]);
-                velX = Double.parseDouble(args[4]);
-                velY = Double.parseDouble(args[5]);
-                velZ = Double.parseDouble(args[6]);
-                mass = Double.parseDouble(args[7]);
-                SpaceObject newSpaceObject = new SpaceObject(name, new Point3D(posX, posY, posZ), mass);
-                newSpaceObject.setVelocity(new Point3D(velX, velY, velZ));
-                spaceObjects.add(newSpaceObject);
-            }
-            reader.close();
+
+        try (Stream<String> input = Files.lines(Paths.get(fileName))) {
+            input.forEach(line -> {
+                Optional<SpaceObject> object = parseLine(line);
+                if (object.isPresent())
+                    spaceObjects.add(object.get());
+            });
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
+
         return spaceObjects;
+    }
+
+    private static Optional<SpaceObject> parseLine(String line) {
+        SpaceObject spaceObject = null;
+        // TODO: handle wrong input
+        if (!line.startsWith("#")) {
+            String[] args = line.split("\\s+");
+            String name = args[0];
+            double posX = Double.parseDouble(args[1]);
+            double posY = Double.parseDouble(args[2]);
+            double posZ = Double.parseDouble(args[3]);
+            double velX = Double.parseDouble(args[4]);
+            double velY = Double.parseDouble(args[5]);
+            double velZ = Double.parseDouble(args[6]);
+            double mass = Double.parseDouble(args[7]);
+            double radius = Double.parseDouble(args[8]);
+
+            spaceObject = new SpaceObject(name, new Point3D(posX, posY, posZ), mass, radius);
+            spaceObject.setVelocity(new Point3D(velX, velY, velZ));
+        }
+
+        return Optional.ofNullable(spaceObject);
     }
 }
