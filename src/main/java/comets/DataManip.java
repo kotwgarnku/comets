f1bb1d72dec0dbb6d6aa6f195ffed7ee978fa448
Paweł Koniarski <me@pkoniarski.com>
2015-05-24 17:50:38 +0200

    Made DataManip class to work and narrowed scope of few variables.

--- src/main/java/comets/DataManip.java
+++ src/main/java/comets/DataManip.java
@@ -27,20 +27,20 @@ public class DataManip {
 
     private static Optional<SpaceObject> parseLine(String line) {
         SpaceObject spaceObject = null;
-        Dictionary<String, Double> params = null;
-        List<String> keys = Arrays.asList("posX", "posY", "posZ", "velX", "velY", "velZ", "mass", "radius");
-        Point3D position, velocity;
         // TODO: handle wrong input
         if (!line.startsWith("#")) {
             String[] args = line.split("\\s+");
             String name = args[0];
+
+            Map<String, Double> params = new HashMap<>();
+            String[] keys = {"posX", "posY", "posZ", "velX", "velY", "velZ", "mass", "radius"};
             int i = 1;
             for(String key : keys) {
                 params.put(key, Double.parseDouble(args[i]));
                 i++;
             }
-            position = new Point3D(params.get("posX"), params.get("posY"), params.get("posZ"));
-            velocity = new Point3D(params.get("velX"), params.get("velY"), params.get("velZ"));
+            Point3D position = new Point3D(params.get("posX"), params.get("posY"), params.get("posZ"));
+            Point3D velocity = new Point3D(params.get("velX"), params.get("velY"), params.get("velZ"));
             spaceObject = new SpaceObject(name, position, params.get("mass"), params.get("radius"));
             spaceObject.setVelocity(velocity);
         }
