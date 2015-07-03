884af90e6c8c3350689a375250414f5c1074c663
Paweł Koniarski <me@pkoniarski.com>
2015-07-03 20:36:44 +0200

    Removed unused classes.

--- src/main/java/comets/Force.java
+++
@@ -1,7 +0,0 @@
-package comets;
-
-import javafx.geometry.Point3D;
-
-public interface Force {
-    public Point3D calculateAcceleration(Point3D thisObjectPos, Point3D otherObjectPos, double otherObjectMass);
-}
